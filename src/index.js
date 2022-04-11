3ca5ce01d64b957fbadec4f914a11ee1c3c3e265
developer-dipali <dipalinikam27@gmail.com>
2022-04-11 23:33:41 +0530

    assingment of post api

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 var bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 
 const route = require('./routes/route.js');
 
@@ -9,6 +11,19 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', route);
+
+mongoose.connect("mongodb+srv://functionUpUranium-2:JECVxS0v96bKoG0a@cluster0.j1yrl.mongodb.net/dipali-DB?retryWrites=true&w=majority",
+{
+    useNewUrlParser : true 
+})
+.then(  () => console.log("MongoDB is Connected"))
+.catch(err => console.log(err))
+
+
+
+
+
+
 
 app.listen(process.env.PORT || 3000, function() {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,15 +1,14 @@
 const express = require('express');
 
 const router = express.Router();
-
-router.get('/test-me', function (req, res) {
-    // let a = { msg: "My first ever API response in JSON !!"} 
-
-
-    res.send( { msg: "My first ever API response in JSON !!"} )
-});
+//  router.get('/test-me', function (req, res) {
+//     // let a = { msg: "My first ever API response in JSON !!"} 
 
 
+//     res.send( { msg: "My first ever API response in JSON !!"} )
+// });
+
+/*
 
 router.get('/test-api1', function (req, res) {
 
@@ -63,8 +62,54 @@ router.post('/test-post2', function (req, res) {
 
 const randomController= require("../controllers/randomController.js")
 //write a post request to accept an element in post request body and add it to the given array and return the new array
-router.post('/test-post3', randomController.addToArray ); //HANDLER/CONTROLLER
+router.post('/test-post3', randomController.addToArray ); //HANDLER/CONTROLLER */
+ 
+
+//assingment
 
 
+let obj =
+   [
+       {
+           "name": "manish",
+           "dob": "1/1/1995",
+           "gender": "male",
+           "city": "jalandhar",
+           "sports": [
+               "swimming"
+           ]
+       },
+       {
+           "name": "gopal",
+           "dob": "1/09/1995",
+           "gender": "male",
+           "city": "delhi",
+           "sports": [
+               "soccer"
+           ],
+       },
+       {
+           "name": "lokesh",
+           "dob": "1/1/1990",
+           "gender": "male",
+           "city": "mumbai",
+           "sports": [
+               "soccer"
+           ],
+       },
+   ]
+ 
+  router.post('/players', function (req, res) {
+   let data =req.body
+    console.log(data)
+    obj.map((x)=>{
+        if(x.name==data.name){
+            return res.send("player already exit")
+        }
+    })
+       obj.push(data)
+       res.send({ newPlayer:obj})
+   });
 
 module.exports = router;
+
