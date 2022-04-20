0d29489b74369737987dbfffe6f0bf5294914ab1
developer-Dipali <dipalinikam27@gmail.com>
2022-04-20 10:14:48 +0530

    middleware function

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
 const app = express();
+const moment = require('moment')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,7 +14,17 @@ mongoose.connect("mongodb+srv://functionUpUranium-2:JECVxS0v96bKoG0a@cluster0.j1
 })
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
+//=====================================///
+app.use (
+    function (req, res, next) {
+       
+        let date = moment().format('YYYY-MM-DD HH:MM:SS')
+        console.log(`${date}, ${req.socket.remoteAddress}, ${req.path}`)
+        next();
+  }
+  );
 
+//=================================//
 app.use('/', route);
 
 
